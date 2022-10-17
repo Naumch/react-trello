3b30b63496517c949d503015984afe690dc7e91b
Anna <anya.naumova.1994@gmail.com>
2022-10-17 10:06:18 +0300

    Добавлена функция добавления карточек

--- src/List.js
+++ src/List.js
@@ -1,17 +1,11 @@
 import React, { useState } from 'react';
 import Card from './Card';
+import uniqid from 'uniqid';
 
 function List({ note, notes, setNotes, listTitle, setListTitle }) {
   const [readyToEdit, setReadyToEdit] = useState(false);
-
-  const result = note.cards.map(card => {
-    return (
-      <Card 
-        key={card.id} 
-        name={card.cardName}
-      />
-    )
-  })
+  const [readyToAddCard, setReadyToAddCard] = useState(false);
+  const [cardName, setCardName] = useState('');
 
   function editTitle(e) {
     if (e.keyCode === 13) {
@@ -26,6 +20,33 @@ function List({ note, notes, setNotes, listTitle, setListTitle }) {
       setListTitle('');
     }
   }
+
+  function addCard(cardName) {
+    setNotes(notes.map(res => {
+      if (note.id === res.id) {
+        let obj = {id: uniqid(), cardName: cardName};
+        return {...res, cards: [...res.cards, obj]};
+      } else {
+        return res;
+      }
+    }))
+  }
+
+  function checkKeydownEnter(e, cardName) {
+    if (e.keyCode === 13) {
+      addCard(cardName);
+      setCardName('');
+    }
+  }
+
+  const result = note.cards.map(card => {
+    return (
+      <Card 
+        key={card.id} 
+        name={card.cardName}
+      />
+    )
+  })
 
   return (
     <div className='list'>
@@ -47,10 +68,42 @@ function List({ note, notes, setNotes, listTitle, setListTitle }) {
           </div>
       }
       {result}
-      <button className='card-add__btn'>
-        <span className='card-add__btn-span'>+</span>
-        Добавить карточку
-      </button>
+      {readyToAddCard
+        ? <div>
+            <textarea 
+              placeholder='Ввести заголовок для этой карточки'
+              className='textarea'
+              value={cardName}
+              onChange={e => setCardName(e.target.value)}
+              onKeyDown={e => checkKeydownEnter(e, cardName)}
+            ></textarea>
+            <button 
+              className='list-add__btn-add'
+              onClick={() => {
+                addCard(cardName);
+                setCardName('');
+              }}
+            >
+              Добавить карточку
+            </button>
+            <button 
+              className='list-add__btn-cancel'
+              onClick={() => {
+                setReadyToAddCard(false);
+                setCardName('');
+              }}
+            >
+              &#10006;
+            </button>
+          </div>
+        : <button 
+            className='card-add__btn'
+            onClick={() => setReadyToAddCard(true)}
+          >
+            <span className='card-add__btn-span'>+</span>
+            Добавить карточку
+          </button>
+      }
     </div>
   )
 }
