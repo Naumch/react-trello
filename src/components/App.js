40915e88cfae05ecc9aebcc5a1017f2a94b4a555
Anna <anya.naumova.1994@gmail.com>
2022-10-20 19:14:53 +0300

    Синхронизация вкладок, сохранение в локальное хранилище

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,48 @@
-import React, { useState } from 'react';
-import uniqid from 'uniqid';
+import React, { useState, useEffect, useRef } from 'react';
 import Main from './Main';
 import Modal from './Modal';
 import { Routes, Route, useLocation } from 'react-router-dom';
 
-const initNotes = [
-  {id: uniqid(), listTitle: 'ToDo', cards: [
-    {id: uniqid(), cardName: 'Лечить Одрюшу', descr: ''}, 
-    {id: uniqid(), cardName: 'Доделать Trello', descr: ''}
-  ]},
-  {id: uniqid(), listTitle: 'Doing', cards: [
-    {id: uniqid(), cardName: 'Лежу на диване', descr: ''}
-  ]}
-]
+function useCrossTabState(stateKey,defaultValue) {
+  const [state,setState] = useState(defaultValue);
+  const isNewSession = useRef(true);
+  
+  useEffect(()=>{
+    if(isNewSession.current){
+      const currentState = localStorage.getItem(stateKey)
+      if(currentState) {
+        setState(JSON.parse(currentState));
+      } else {
+        setState(defaultValue);
+      }
+      isNewSession.current=false;
+      return
+    }
+
+    try{
+      localStorage.setItem(stateKey,JSON.stringify(state));
+    } catch(error) {}
+
+  }, [state, stateKey, defaultValue]);
+
+  useEffect(()=>{
+    const onReceieveNotes = (e) => {
+      const {key, newValue} = e;
+      if (key === stateKey) {
+        setState(JSON.parse(newValue))
+      } 
+    }
+      
+    window.addEventListener('storage', onReceieveNotes);
+    return () => window.removeEventListener('storage', onReceieveNotes);
+    }, [stateKey,setState])
+
+  return [state,setState];
+}
 
 function App() {
-  const [notes, setNotes] = useState(initNotes);
+  const [notes, setNotes] = useCrossTabState("notes", []);
+
   const location = useLocation();
   const background = location.state && location.state.background;
 
